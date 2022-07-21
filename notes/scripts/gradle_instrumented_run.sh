7f6f05abbbac9d06c0ef831af054335373b0ac32
Zarir Hamza <zarir.hamza@datadoghq.com>
2022-07-21 10:24:33 -0400

    Fixed gradle instrumented run script
    Fixed gradle script to run with automatic instrumentation
    Removed previous script that ran an executable
    Replaced with jar file being run with environment variables

--- notes/scripts/gradle_instrumented_run.sh
+++ notes/scripts/gradle_instrumented_run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-./gradlew shadowJar
-env JAVA_OPTS='-javaagent:../dd-java-agent.jar -XX:FlightRecorderOptions=stackdepth=256 -Ddd.trace.sample.rate=1 -Ddd.service=notes -Ddd.env=staging -Ddd.version=0.0.1' build/libs/notes-0.0.1-SNAPSHOT-all.jar
-
+./gradlew clean shadowJar
+java -javaagent:../dd-java-agent.jar -Ddd.trace.sample.rate=1 -Ddd.service=notes -Ddd.env=dev -jar -Ddd.version=0.0.1 build/libs/notes-0.0.1-SNAPSHOT-all.jar
