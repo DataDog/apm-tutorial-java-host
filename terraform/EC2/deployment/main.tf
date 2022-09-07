ed25de55e64a0d85510474c0f690eabb1986acec
Zarir Hamza <zarir.hamza@datadoghq.com>
2022-09-07 12:35:36 -0400

    Add EC2 tf files w/o changing for java

---
+++ terraform/EC2/deployment/main.tf
@@ -0,0 +1,123 @@
+module "settings" {
+    source = "../global_constants"
+}
+
+provider "aws" {
+  region  = module.settings.aws_region
+  profile = module.settings.aws_profile
+}
+
+resource "aws_ecs_cluster" "apm_tutorial_cluster" {
+  name = "apm-tutorial-ec2" # Naming the cluster
+}
+
+resource "aws_ecs_task_definition" "notes_task" {
+  family                   = "notes" # Naming our first task
+  container_definitions    = jsonencode([
+    {
+      name: "notes",
+      image: "${module.settings.aws_ecr_repository}:notes_app",
+      essential: true,
+      portMappings: [
+        {
+          containerPort: 8080,
+          hostPort: 8080
+        }
+      ],
+      memory: 512,
+      cpu: 256,
+      environment: [
+        {
+            name: "CALENDAR_HOST",
+            value: "localhost:9090"
+        },
+        {
+            name: "DD_SERVICE",
+            value: "Notes"
+        },
+        {
+            name: "DD_ENV",
+            value: "dev"
+        },
+        {
+            name: "DD_VERSION",
+            value: "0.1.0"
+        }
+      ]
+    }
+  ])
+  requires_compatibilities = ["EC2"] # Stating that we are using ECS 
+  network_mode             = "host"    # Using awsvpc as our network mode as this is required
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_ecs_service" "notes_service" {
+  name            = "notes-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.apm_tutorial_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.notes_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "EC2"
+  desired_count   = 1 # Setting the number of containers to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.notes_task.family}"
+    container_port   = 8080 # Specifying the container port
+  }
+}
+
+## ----------------------------------------------- Calendar Service ------------------------------------------------------------------------------------------
+
+
+resource "aws_ecs_task_definition" "calendar_task" {
+  family                   = "calendar" # Naming our first task
+  container_definitions    = jsonencode([
+    {
+      name: "calendar",
+      image: "${module.settings.aws_ecr_repository}:calendar_app",
+      essential: true,
+      environment: [
+        {
+            name: "DD_SERVICE",
+            value: "Calendar"
+        },
+        {
+            name: "DD_ENV",
+            value: "dev"
+        },
+        {
+            name: "DD_VERSION",
+            value: "0.1.0"
+        }
+      ],
+      portMappings: [
+        {
+          containerPort: 9090,
+          hostPort: 9090
+        }
+      ],
+      memory: 512,
+      cpu: 256,
+    }
+  ])
+  requires_compatibilities = ["EC2"] # Stating that we are using ECS 
+  network_mode             = "host"    # Using awsvpc as our network mode as this is required
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_ecs_service" "calendar_service" {
+  name            = "calendar-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.apm_tutorial_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.calendar_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "EC2"
+  desired_count   = 1 # Setting the number of containers to 1
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group_2.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.calendar_task.family}"
+    container_port   = 9090 # Specifying the container port
+  }
+}
---
+++ terraform/EC2/user_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo ECS_CLUSTER=apm-tutorial-ec2 >> /etc/ecs/ecs.config
